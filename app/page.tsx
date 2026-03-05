0deca87c6f69eda0f38aa8c32ef8360cadbb3a97
luna1516 <lunakenny345@gmail.com>
2026-03-05 19:05:47 +0000

    Implemented first API call

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
 import Image from "next/image";
 
 export default function Home() {
+  const api = async () => {
+    const response = await fetch("https://api1.raildata.org.uk/1010-live-departure-board-dep1_2/LDBWS/api/20220120/GetDepBoardWithDetails/BHM", {
+      method: "GET",
+      headers: {
+        "x-apikey": "LlVehV3Eq50aKh8InKkjsOl6AVzjKFV4a6RoSGDChmwzOvU5"
+      }
+    })
+    return JSON.stringify(await response.json())
+  }
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -16,6 +25,7 @@ export default function Home() {
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file.
           </h1>
+          {api()}
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
             <a
